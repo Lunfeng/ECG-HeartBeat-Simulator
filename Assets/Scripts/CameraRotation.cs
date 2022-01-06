414bc864f943171cdb8d545911bf775921af9e0c
Lunfeng <1427208963@qq.com>
2022-01-06 12:34:12 +0800

    摄像机面向心脏

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+
+    public GameObject Heart;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class CameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Camera.main.gameObject.transform.LookAt(Heart.transform);
     }
 }
